6ff8c70c9f83dee3bb6f7e9a20a0243e6404704c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-25 14:33:04 +0000

    Deploy bougieL/bougie-design to github.com/bougieL/bougie-design.git:gh-pages

--- scripts/prerender.js
+++ scripts/prerender.js
@@ -2,62 +2,35 @@ const path = require('path')
 const fs = require('fs')
 const {
   createSPAServer,
-  Renderer,
+  renderUrlsToString,
   getRelativePathFromUrl,
+  getUrlsFromSite,
   writeFile
 } = require('@bougiel/puppeteer-prerenderer')
 
-const dist = path.resolve(__dirname, '../dist')
+const dist = path.resolve(__dirname, '../.docz')
 const port = 4200
 
 const server = createSPAServer({
-  path: dist,
+  dist,
   port,
   base: '/bougie-design',
-  callback: renderUrls
+  callback: render
 })
 
-async function renderUrls () {
-  const renderer = new Renderer()
-  await renderer.init()
-  for (const url of getUrls()) {
-    await render(url, renderer)
-  }
-  server.close()
-  console.log(`render completed.`)
-  process.exit(0)
-}
-
-async function render (url, renderer) {
-  const content = await renderer.renderToString(url)
-  const dir = getRelativePathFromUrl(url).replace('/hiui', '')
-  writeFile(path.join(dist, dir), content)
-  console.log(`rendered ${url}`)
-}
-
-function getUrls () {
-  const urls = [...getComponentUrls(), ...getTemplateUrls(), ...getExtraUrls()]
-  const zh = urls.map(v => `http://localhost:${port}/hiui/zh-CN/${v}`)
-  const en = urls.map(v => `http://localhost:${port}/hiui/en-US/${v}`)
-  return [...zh, ...en]
-}
-
-function getComponentUrls () {
-  const compPath = path.resolve(__dirname, '../docs/zh-CN/components')
-  return fs.readdirSync(compPath).map(v => `docs/${v.replace('.md', '')}`)
-}
-
-function getTemplateUrls () {
-  const tempPath = path.resolve(__dirname, '../docs/zh-CN/templates')
-  return fs.readdirSync(tempPath).map(v => `templates/${v.replace('.md', '')}`)
-}
-
-function getExtraUrls () {
-  return [
-    'docs/quick-start',
-    'docs/template',
-    'docs/theme',
-    'docs/i18n',
-    'docs/changelog'
-  ]
+function render() {
+  getUrlsFromSite(`http://localhost:${port}/bougie-design`, urls => {
+    renderUrlsToString({
+      urls,
+      callback(content, url) {
+        const rp = getRelativePathFromUrl(url)
+        const p = path.join(dist, rp)
+        writeFile(p, content)
+      },
+      onFinished() {
+        server.close()
+        process.exit(0)
+      }
+    })
+  })
 }
